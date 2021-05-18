be0f21a0304b687e84935ba617c9b16bbd28a85e
Misha Brukman <mbrukman@google.com>
2021-05-17 22:14:55 -0400

    Implement atoi(), atol(), atoll() with tests
    Also add remaining function declarations from `stdlib.h` to make it easy to
    implement missing functions easier in the future.
    
    Update `.gitignore` to ignore all built test binaries.

--- lib/stdlib.c
+++ lib/stdlib.c
@@ -14,6 +14,132 @@
 
 #include <stdlib.h>
 
+////////////////////////////////////////////////////////////////////////////////
+// 7.20.1 Numeric conversion functions
+////////////////////////////////////////////////////////////////////////////////
+
+// 7.20.1.1 The atof function
+double atof(const char *nptr);
+
+// 7.20.1.2 The atoi, atol, and atoll functions
+int atoi(const char *nptr) {
+  int negative = 0;
+  int value = 0;
+
+  if (nptr[0] == '-') {
+    negative = 1;
+    ++nptr;
+  }
+
+  while (nptr != NULL) {
+    int digit = 0;
+    if ('0' <= *nptr && *nptr <= '9') {
+      digit = *nptr - '0';
+    } else {
+      break;
+    }
+    value = value * 10 + digit;
+    ++nptr;
+  }
+
+  return negative ? -value : value;
+}
+
+long int atol(const char *nptr) {
+  int negative = 0;
+  long int value = 0;
+
+  if (nptr[0] == '-') {
+    negative = 1;
+    ++nptr;
+  }
+
+  while (nptr != NULL) {
+    int digit = 0;
+    if ('0' <= *nptr && *nptr <= '9') {
+      digit = *nptr - '0';
+    } else {
+      break;
+    }
+    value = value * 10 + digit;
+    ++nptr;
+  }
+
+  return negative ? -value : value;
+}
+
+long long int atoll(const char *nptr) {
+  int negative = 0;
+  long long int value = 0;
+
+  if (nptr[0] == '-') {
+    negative = 1;
+    ++nptr;
+  }
+
+  while (nptr != NULL) {
+    int digit = 0;
+    if ('0' <= *nptr && *nptr <= '9') {
+      digit = *nptr - '0';
+    } else {
+      break;
+    }
+    value = value * 10 + digit;
+    ++nptr;
+  }
+
+  return negative ? -value : value;
+}
+
+// 7.20.1.3 The strtod, strtof, and strtold functions
+double strtod(const char * restrict nptr, char ** restrict endptr);
+float strtof(const char * restrict nptr, char ** restrict endptr);
+long double strtold(const char * restrict nptr, char ** restrict endptr);
+
+// 7.20.1.4 The strtol, strtoll, strtoul, and strtoull functions
+long int strtol(const char * restrict nptr, char ** restrict endptr, int base);
+long long int strtoll(const char * restrict nptr, char ** restrict endptr, int base);
+unsigned long int strtoul(const char * restrict nptr, char ** restrict endptr, int base);
+unsigned long long int strtoull(const char * restrict nptr, char ** restrict endptr, int base);
+
+////////////////////////////////////////////////////////////////////////////////
+// 7.20.2 Pseudo-random sequence generation functions
+////////////////////////////////////////////////////////////////////////////////
+
+// 7.20.2.1 The rand function
+int rand(void);
+
+// 7.20.2.2 The srand function
+void srand(unsigned int seed);
+
+////////////////////////////////////////////////////////////////////////////////
+// 7.20.3 Memory management functions
+////////////////////////////////////////////////////////////////////////////////
+
+// 7.20.3.1 The calloc function
+void *calloc(size_t nmemb, size_t size);
+
+// 7.20.3.2 The free function
+void free(void *ptr);
+
+// 7.20.3.3 The malloc function
+void *malloc(size_t size);
+
+// 7.20.3.4 The realloc function
+void *realloc(void *ptr, size_t size);
+
+////////////////////////////////////////////////////////////////////////////////
+// 7.20.4 Communication with the environment
+////////////////////////////////////////////////////////////////////////////////
+
+// 7.20.4.1 The abort function
+void abort(void);
+
+// 7.20.4.2 The atexit function
+int atexit(void (*func)(void));
+
+// 7.20.4.3 The exit function
+
 // Relevant docs:
 //
 // * How system calls work on Linux (covers 32-bit and 64-bit):
@@ -58,3 +184,55 @@ void exit(int status) {
 #  error "This OS is not 64-bit Linux or macOS (not yet supported)."
 #endif
 }
+
+// 7.20.4.4 The _Exit function
+void _Exit(int status);
+
+// 7.20.4.5 The getenv function
+char *getenv(const char *name);
+
+// 7.20.4.6 The system function
+int system(const char *string);
+
+////////////////////////////////////////////////////////////////////////////////
+// 7.20.5 Searching and sorting utilities
+////////////////////////////////////////////////////////////////////////////////
+
+// 7.20.5.1 The bsearch function
+void *bsearch(const void *key, const void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
+
+// 7.20.5.2 The qsort function
+void qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
+
+////////////////////////////////////////////////////////////////////////////////
+// 7.20.6 Integer arithmetic functions
+////////////////////////////////////////////////////////////////////////////////
+
+// 7.20.6.1 The abs, labs and llabs functions
+int abs(int j);
+long int labs(long int j);
+long long int llabs(long long int j);
+
+// 7.20.6.2 The div, ldiv, and lldiv functions
+div_t div(int numer, int denom);
+ldiv_t ldiv(long int numer, long int denom);
+lldiv_t lldiv(long long int numer, long long int denom);
+
+////////////////////////////////////////////////////////////////////////////////
+// 7.20.7 Multibyte/wide character conversion functions
+////////////////////////////////////////////////////////////////////////////////
+
+// 7.20.7.1 The mblen function
+int mblen(const char *s, size_t n);
+
+// 7.20.7.2 The mbtowc function
+int mbtowc(wchar_t * restrict pwc, const char * restrict s, size_t n);
+
+// 7.20.7.3 The wctomb function
+int wctomb(char *s, wchar_t wc);
+
+// 7.20.8.1 The mbstowcs function
+size_t mbstowcs(wchar_t * restrict pwcs, const char * restrict s, size_t n);
+
+// 7.20.8.2 The wcstombs function
+size_t wcstombs(char * restrict s, const wchar_t * restrict pwcs, size_t n);
